8d4587397c3085a75aaa45bd2347cf6e3e2af374
Sajalhowlader <sajolhowlader08@gmail.com>
2022-04-29 01:22:14 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Home/Header/Header';
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
