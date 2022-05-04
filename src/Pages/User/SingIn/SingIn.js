d84429221f4d94f3c5893cb5d0c12a34ac63b381
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-04 15:59:57 +0600

    add singIn component

--- src/Pages/User/SingIn/SingIn.js
+++ src/Pages/User/SingIn/SingIn.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import bgImg from '../../../images/bg.svg'
 import userImg from '../../../images/avatar.svg'
+import googleImg from '../../../images/google3.png'
+import { FaKey, FaUser } from 'react-icons/fa';
 const SingIn = () => {
     const navigate = useNavigate()
     const handleSingUp = () => {
@@ -16,10 +18,25 @@ const SingIn = () => {
                 <div className="divider-info">
                     <img src={userImg} alt="" className="user-img" />
                     <h2>Welcome</h2>
-                    <input type="email" name="" />
-                    <input type="password" name="" />
-                    <p>New to C&A Werehouse?<strong onClick={handleSingUp}>Register Now</strong></p>
+                    <form>
+                        <span className='d-flex align-items-center user-input'> <FaUser className='me-2' />
+                            <input className='' placeholder='User Email' type="email" name="" />
+                        </span>
+                        <span className='d-flex align-items-center user-input '>
+                            <FaKey className='me-2' />
+                            <input className='' type="password" name="" placeholder='User Password' />
+                        </span>
+                        <p>New to C&A Werehouse?<strong className='strong' onClick={handleSingUp}>Register Now</strong></p>
+                        <input className='submit-type' type="submit" value="Done" />
+                    </form>
+                    <div className="or-container">
+                        <div></div>
+                        <strong>or</strong>
+                        <div></div>
+                    </div>
+                    <button className='google-btn'> <img src={googleImg} alt="" /> Continue With</button>
                 </div>
+
             </div>
         </div>
     );
